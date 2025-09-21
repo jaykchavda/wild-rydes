2efb01f6f3839de1a00965d1661ca276b7dd0448
Jay <109908594+jaykchavda@users.noreply.github.com>
2025-09-21 06:07:44 +0000

    giadd the config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_nFd5rA1nV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '288k0smbhb7jlgd8m6ii4u4s9r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
